750b7105efb27c1b04562293ec937f903d910ad5
svit.darkly <svit.darkly@gmail.com>
2023-12-30 16:34:16 +0000

    ops: added svg import as react component

--- components/ui/topbar.tsx
+++ components/ui/topbar.tsx
@@ -1,21 +1,24 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Link from "next/link";
+import SkattjaktLogo from "@/assets/skattjakt-logo.svg";
 
 export const Topbar = () => {
   return (
-    <div className="flex w-full py-3 px-4">
-      <div className="p-2">
-        <Link href={"/generator"} className="w-full">
-          <div className="hover:bg-secondary rounded-md p-2 w-full">
-            <div className="w-6 h-6 bg-secondary-foreground rounded-[6px]"></div>
-          </div>
-        </Link>
-      </div>
-      <div className="w-full flex items-center justify-center">
-        <h1 className="text-2xl w-full text-center font-bold">Skattjakt</h1>
-      </div>
-      <div className="p-2">
-        <ModeToggle />
+    <div className="sticky top-0 w-full">
+      <div className="flex h-20 w-full py-3 px-4 bg-background/80 backdrop-blur-xl items-center">
+        <div>
+          <Link href={"/generator"} className="w-full">
+            <div className="hover:bg-secondary rounded-md p-2 w-full">
+              <SkattjaktLogo />
+            </div>
+          </Link>
+        </div>
+        <div className="w-full flex items-center justify-center">
+          <h1 className="text-2xl w-full text-center font-bold">Skattjakt</h1>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
