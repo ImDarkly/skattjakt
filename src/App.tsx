0cb1473397035a9f98eccabe61554d111422c4ef
ImDarkly <svit.darkly@gmail.com>
2024-02-26 17:31:23 +0200

    chore: added missing enter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layout from '@/lib/layout';
 import Routings from '@/lib/router/Routings';
+
 const isProduction = process.env.NODE_ENV === 'production';
 
 const App = () => (
