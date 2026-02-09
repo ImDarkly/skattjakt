d7335d40b5f3ca7dc86092b526ff0f712188b790
ImDarkly <svit.darkly@gmail.com>
2026-02-09 17:17:49 +0200

    chore: redesigned footer

--- src/lib/components/footer.tsx
+++ src/lib/components/footer.tsx
@@ -14,46 +14,48 @@ const isProduction = process.env.NODE_ENV === 'production';
 const version = APP_VERSION;
 export const Footer = () => {
   return (
-    <footer className="flex h-24 w-full items-center justify-center grow gap-1 bg-background">
-      <p>Made with</p>
-      <TooltipProvider>
-        <Tooltip delayDuration={5000}>
-          <TooltipTrigger asChild>
-            <Icon
-              icon="heroicons:heart-16-solid"
-              className="light:hover:drop-shadow-[0px_0px_8px_rgba(244,63,94,1)] text-2xl text-primary transition-all duration-500 hover:drop-shadow-[0px_0px_8px_rgba(225,29,72,1)]"
-            />
-          </TooltipTrigger>
-          <TooltipContent
-            className="bg-primary text-primary-foreground"
-            side="top"
-          >
-            <p>
-              Version @{version}
-              {isProduction ? '' : 'dev'}
-            </p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <p>by</p>
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link
-              to="https://github.com/ImDarkly"
-              className="font-bold text-primary hover:text-primary/90"
+    <footer className="flex h-24 w-full items-center justify-between grow gap-1 bg-background px-4">
+      <div className="flex">
+        <p>Made with</p>
+        <TooltipProvider>
+          <Tooltip delayDuration={5000}>
+            <TooltipTrigger asChild>
+              <Icon
+                icon="heroicons:heart-16-solid"
+                className="light:hover:drop-shadow-[0px_0px_8px_rgba(244,63,94,1)] text-2xl text-primary transition-all duration-500 hover:drop-shadow-[0px_0px_8px_rgba(225,29,72,1)]"
+              />
+            </TooltipTrigger>
+            <TooltipContent
+              className="bg-primary text-primary-foreground"
+              side="top"
             >
-              ImDarkly
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent
-            className="bg-popover-foreground text-popover"
-            side="top"
-          >
-            <p>https://github.com/ImDarkly</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+              <p>
+                Version @{version}
+                {isProduction ? '' : 'dev'}
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <p>by</p>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                to="https://github.com/ImDarkly"
+                className="font-bold text-primary hover:text-primary/90"
+              >
+                ImDarkly
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent
+              className="bg-popover-foreground text-popover"
+              side="top"
+            >
+              <p>https://github.com/ImDarkly</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <ModeToggle />
     </footer>
   );
