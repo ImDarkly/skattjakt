5537a5098fc78abf94497ab2aaaadd03d9f70493
ImDarkly <svit.darkly@gmail.com>
2026-02-09 17:12:22 +0200

    chore: redesign clear items button

--- src/lib/components/items/clear-items-button.tsx
+++ src/lib/components/items/clear-items-button.tsx
@@ -1,12 +1,13 @@
 import { useBoundStore } from '@/lib/zustand/store';
 import { Button } from '../ui/button';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function ClearItemsButton() {
   const { resetItems } = useBoundStore();
 
   return (
-    <Button variant="destructive" onClick={resetItems}>
-      Clear All Items
+    <Button size="icon" variant="destructive" onClick={resetItems}>
+      <Icon width={24} icon="heroicons:trash-16-solid" />
     </Button>
   );
 }
